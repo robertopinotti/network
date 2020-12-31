42e35402fb636a313ae86f241e78181c8745e56c
GitHub <github-action@users.noreply.github.com>
2020-12-31 21:10:47 +0000

    deploy files

--- website/src/Common.ts
+++
@@ -1,8 +0,0 @@
-
-export const range = (start:number, end:number, step = 1) => {
-  let output = [];
-  for (let i = start; i < end; i += step) {
-    output.push(i);
-  }
-  return output;
-};
--- website/src/models/ExtraGroup.ts
+++
@@ -1,7 +0,0 @@
-export default interface ExtraGroup
-{
-    name?:string,
-    description?:string,
-    image?:string,
-    gruppo?:string
-}
